5930e8076736110590e6bd48f6bc10347f45f525
WillianMR <58883121+WillianMR@users.noreply.github.com>
2022-07-19 14:12:04 -0300

    configurando rotas

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -4,13 +4,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
     <div class="column is-tree-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa" />
-      <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
-        <Box v-if="listaEstaVazia">
-          Você ainda não tem tarefas!
-        </Box>
-      </div>
+      <router-view></router-view>
     </div>
     
   </main>
@@ -19,30 +13,19 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
-import Formulario from './components/Formulario.vue';
-import Tarefa from './components/Tarefa.vue';
-import ITarefa from './interfaces/ITarefa';
-import Box from './components/Box.vue';
+
 
 
 export default defineComponent({
     name: "App",
-    components: { BarraLateral, Formulario, Tarefa, Box },
+    components: { BarraLateral },
     data () {
       return {
-        tarefas: [] as ITarefa[],
         modoEscuro: false
       }
     },
-    computed: {
-      listaEstaVazia(): boolean{
-        return this.tarefas.length === 0
-      }
-    },
     methods: {
-      salvarTarefa (tarefa: ITarefa){
-        this.tarefas.push(tarefa)
-      },
+
       trocarTema(modoEscuro: boolean){
         this.modoEscuro = modoEscuro
       }
