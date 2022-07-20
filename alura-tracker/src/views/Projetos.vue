02a8fe749107a3dc8267c4c70d289016cdb1b0a7
WillianMR <58883121+WillianMR@users.noreply.github.com>
2022-07-20 13:44:26 -0300

    completando funcionalidades

--- alura-tracker/src/views/Projetos.vue
+++ alura-tracker/src/views/Projetos.vue
@@ -1,66 +1,19 @@
 <template>
-    <section class="projetos">
-        <h1 class="title">Projetos</h1>
-        <form @submit.prevent="salvar">
-            <div class="field">
-                <label for="nomeDoProjeto" class="label">
-                    Nome do Projeto
-                </label>
-                <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto">
-            </div>
-            <div class="field">
-                <button class="button" type="submit">Salvar</button>
-            </div>
-        </form>
-        <table class="table is-fullwidth">
-            <thead>
-                <tr>
-                    <th>
-                        ID
-                    </th>
-                    <th>
-                        Nome
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="projeto in projetos" :key="projeto.id">
-                    <td>{{ projeto.id }}</td>
-                    <td>{{ projeto.nome }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </section>
+  <div class="projetos">
+    <h1 class="title">Projetos</h1>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import IProjeto from '../interfaces/IProjeto';
-
+import { defineComponent } from 'vue'
 export default defineComponent({
-    name: "Projetos-View",
-    data () {
-        return {
-            nomeDoProjeto: "",
-            projetos: [] as IProjeto[]
-        };
-    },
-    methods: {
-        salvar () {
-            const projeto: IProjeto = {
-                nome: this.nomeDoProjeto,
-                id: new Date().toISOString()
-            }
-            this.projetos.push(projeto)
-            this.nomeDoProjeto = ''
-        }
-    }
+  name: 'Projetos-View'
 })
 </script>
 
 <style scoped>
 .projetos {
-    padding: 1.25rem;
+  padding: 1.25rem;
 }
-
 </style>
